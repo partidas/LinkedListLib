cd9c4ef756417f0ee891b7a98d586c7eaeec737f
Steven P <ste94par@gmail.com>
2021-07-28 23:00:27 -0700

    edited deleteNode function (not deleteMid) - cannot delete tail will either need to adjust the findNode function or manually find Node in deleteNode function

--- LinkedList2-3.c
+++ LinkedList2-3.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* ***NOTES***
+newNode -> returns address of the memory allocated
+*newNode -> returns the value pointed to on the heap (memory)
+&newNode -> returns the pointer address on the stack (temporary within the function call)
+*/
 
 typedef struct Node {    /* What the node is:
        Pointer to next node
@@ -15,11 +20,6 @@ Node * createNode (int d) {
     newNode->next = NULL;
     newNode->data = d;
     return newNode;
-    /*
-    newNode -> returns address of the memory allocated
-    *newNode -> returns the value pointed to on the heap (memory)
-    &newNode -> returns the pointer address on the stack (temporary within the function call)
-    */
 }
 //return status if memory runs out
 int appendToTail(Node* head, int d) {
@@ -46,17 +46,41 @@ Node * findNode(Node * head, int d) {
     return NULL;
 }
 
-//Deletes node but not head tail
-void deleteNode(Node* n) {
-    if(n->next != NULL) {
-        Node* temp = n->next;
+//Deletes a node from LinkedList.
+//Input - int n - the Node with value n that needs to be removed.
+//Input - Node * head - pointer to the linkedList
+//Returns 1 or 0 - 1 if found and deleted, 0 if it did not find. -1 if error
+//deleteNode calls on findNode and then performs the delete.
+
+/*NOTE for MAX- does not work for removing the tail, need to either
+         update the findNode method to return the node before, since
+         the next pointer on that node needs to be set to null;
+         or run a finde node manually without a call to it.
+*/
+int deleteNode(Node * head, int n) {
+    printf("Deleting Node with value %d...\n", n);
+    Node * nNode = findNode(head, n);
+    if(nNode == NULL) {
+        printf("Does not exist\n");
+        return 0;
     }
-    n->data = n->next->data;
-    n->next = n->next->next;
-    // dealloc temp <-- if allocating to begin with
+    printf("Node found! %d\n", nNode->data);
+    //if Node to delete is the tail:
+    if(nNode->next == NULL) {
+        nNode = NULL;
+    } else { //if next node is not null - not a real delete, 
+             //more of a delete data and remove next node
+        nNode->data = nNode->next->data; //copies the data of next node into current
+        Node * temp = nNode->next; //temp node so we can free the deleted node
+        nNode->next = nNode->next->next; //set next node to next->next node (can be null)
+        free(temp); //free node from memory
+        return 1; //return 1 for success
+    }
+    printf("Error deleting node...\n");
+    return -1; //if all of the above failed, then report an error
 }
 
-//assign type def struct --> todo
+//2.3
 void deleteMid(Node* n) {
     Node* temp = n->next;
 /*    if(n->next != NULL) {
@@ -87,6 +111,9 @@ void printList(Node* n) {
     printf("\nFinished!\n");
 }
 
+
+
+//function to create auto testing
 void testing(int size) {
     //Create a LinkedList of Size 5;
     int value = 1;
@@ -98,7 +125,7 @@ void testing(int size) {
         value++;
     }
     printList(test1);
-    Node * nodeToDelete = findNode(test1, 7)
+    Node * nodeToDelete = findNode(test1, 7);
     printf("testing");
 }
 
@@ -111,7 +138,7 @@ int main() {
     appendToTail(linked1, 6);
     printList(linked1);
     Node * nodeToDelete = findNode(linked1, 4);
-    deleteMid(nodeToDelete);
+    deleteNode(linked1, 4);
     printList(linked1);
     
     return 0;
