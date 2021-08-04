6191ab04606ce0fae5cbcd8a0fa5f6b576a00704
Steven P <ste94par@gmail.com>
2021-08-03 23:59:01 -0700

    updated the deleteNode method, included size and isHead fields for struct node. sizing works. implemented find kth to last element function, works.

--- LinkedList2-3.c
+++ LinkedList2-3.c
@@ -13,24 +13,29 @@ typedef struct Node {    /* What the node is:
     */
     struct Node* next;
     int data;
-    int isHead;
     int size;
+    int head;
 }Node;
 
-Node * createNode (int d) {
+Node * createNode (int d, int isHead) {
     Node * newNode = malloc(sizeof(Node));
     newNode->next = NULL;
     newNode->data = d;
+    newNode->head = isHead;
+    if(isHead = 1) {
+        newNode->size = 1;
+    }
     return newNode;
 }
 
 //return status if memory runs out
 int appendToTail(Node* head, int d) {
-    Node* end = createNode(d);
+    Node* end = createNode(d, 0);
     if(end == NULL) {
         return -1;
     }
     Node* iter = head;
+    head->size += 1;
     while (iter->next != NULL) {
         iter = iter->next;
     }
@@ -54,8 +59,6 @@ Node * findNode(Node * head, int d) {
 //Input - int n - the Node with value n that needs to be removed.
 //Input - Node * head - pointer to the linkedList
 //Returns 1 or 0 - 1 if found and deleted, 0 if it did not find. -1 if error
-//deleteNode calls on findNode and then performs the delete.
-
 int deleteNode(Node * head, int n) {
     printf("Deleting Node with value %d...\n", n);
     
@@ -71,10 +74,11 @@ int deleteNode(Node * head, int n) {
         } else { //instead of deleting the node, we will update the head node's
                  //data to the next node's and delete the next node.
                  // this is to keep the head node pointer, since we return int.
-            head->data = head->next->data;
-            iter = head->next;
-            head->next = head->next->next;
-            free(iter);
+            iter->data = iter->next->data;
+            temp = iter->next;
+            iter->next = iter->next->next;
+            head->size -= 1;
+            free(temp);
             return 1; //return the new head
         }
     }
@@ -87,6 +91,7 @@ int deleteNode(Node * head, int n) {
             printf("Node found and deleted! %d\n", iter->next->data);
             temp = iter->next;
             iter->next = iter->next->next;
+            head->size -= 1;
             free(temp); 
             return 1; 
         }
@@ -94,6 +99,20 @@ int deleteNode(Node * head, int n) {
     }
     printf("Error deleting node...%d not in linked list!\n", n);
     return -1; //if all of the above failed, then report an error
+}
+
+
+//print out linked list
+void printList(Node* n) {
+    Node* iter = n;
+    printf("%d ", iter->data);
+    iter = iter->next;
+    while(iter != NULL) {
+        printf("->");
+        printf(" %d ", iter->data);
+        iter= iter->next;    
+    }
+    printf("\nLinked List Printed!\n");
 }
 
 //2.3
@@ -114,19 +133,27 @@ void deleteMid(Node* n) {
     }
 }
 
-//print out linked list
-void printList(Node* n) {
-    Node* iter = n;
-    printf("%d ", iter->data);
-    iter = iter->next;
-    while(iter != NULL) {
-        printf("->");
-        printf(" %d ", iter->data);
-        iter= iter->next;    
+//Find and return the kth to last element of singly linked list
+int findKth(Node * head, int k) {
+    if(k > head->size) {
+        printf("%dth to last element does not exist!!\n", k);
+        return -1;
     }
-    printf("\nLinked List Printed!\n");
-}
 
+    int index = head->size - k;
+    int iter = 0;
+    Node * linkedIter= head;
+    while(iter != index) {
+        linkedIter = linkedIter->next;
+        iter++;
+    }
+    if(iter == index) {
+        return linkedIter->data;
+    } else {
+        printf("Error finding Kth to last element\n");
+        return -1;
+    }
+}
 
 
 //function to create auto testing
@@ -134,7 +161,7 @@ void printList(Node* n) {
 void testing(int size) {
     //Create a LinkedList of Size 5;
     int value = 1;
-    Node * test1 = createNode(value);
+    Node * test1 = createNode(value, 1);
     value++;
 
     while(value != size) {
@@ -146,22 +173,25 @@ void testing(int size) {
     printf("testing");
 }
 
-
-
 int main() {
-    Node * linked1 = createNode(5);
+    Node * linked1 = createNode(5, 1);
+    printf("LinkedList size: %d\n", linked1->size);
     appendToTail(linked1, 4);
     appendToTail(linked1, 6);
     // linked1: 5 -> 4 -> 6
-    printList(linked1);
+    /*printList(linked1);
+    printf("LinkedList size: %d\n", linked1->size);
     Node * nodeToDelete = findNode(linked1, 4);
     deleteNode(linked1, 6);
+    printf("LinkedList size: %d\n", linked1->size);
     // linked1: 5 -> 4
     printList(linked1);
     appendToTail(linked1, 7);
+    printf("LinkedList size: %d\n", linked1->size);
     // linked1: 5 -> 4 -> 7
     printList(linked1);
     deleteNode(linked1, 5);
+    printf("LinkedList size: %d\n", linked1->size);
     // linked1: 4 -> 7
     printList(linked1);
     appendToTail(linked1, 5);
@@ -172,8 +202,20 @@ int main() {
     deleteNode(linked1, 4);
     printList(linked1);
     deleteNode(linked1, 5);
+    printf("LinkedList size: %d\n", linked1->size);
     printList(linked1);
     deleteNode(linked1, 7);
-    deleteNode(linked1, 4); 
+    deleteNode(linked1, 4); */
+    appendToTail(linked1, 9);
+    appendToTail(linked1, 15);
+    appendToTail(linked1, 1);
+    printList(linked1);
+    printf("LinkedList size: %d\n", linked1->size);
+
+    printf("\n3rd to last element: %d\n", findKth(linked1, 3));
+    printf("\n2nd to last element: %d\n", findKth(linked1, 2));
+    printf("\n5th to last element: %d\n", findKth(linked1, 5));
+    printf("\n6th to last element: %d\n", findKth(linked1, 6));
+    printf("\n7th to last element: %d\n", findKth(linked1, 7));
     return 0;
 }
